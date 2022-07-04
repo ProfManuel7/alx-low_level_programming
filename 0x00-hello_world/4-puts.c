7b3c3cf2e2832c41d866b5c2d75e92db243d09cd
ProfManuel7 <profmanuel7@gmail.com>
2022-07-04 14:49:35 -0700

    a c program that writes after a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
-int main(void)
+
 /**
- *Description: prints out a string to sdtout
- *Return: 0 if successful
- */
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
+int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
