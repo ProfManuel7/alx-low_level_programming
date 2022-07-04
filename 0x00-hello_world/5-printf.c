2e25e368d26fa6daa3d4428bd09e2fbf4126417e
ProfManuel7 <profmanuel7@gmail.com>
2022-07-04 15:45:06 -0700

    c program that prints out followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: prints ouut a string to stdout
+ * Return 0 if successful
+ */
+int main(void)
+{
+printf("with proper grammar,but the outcome is a piece of art,\n");
+return (0);
+}
