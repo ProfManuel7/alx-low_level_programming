1c4204bcca6ee9975eeef3e5294a4c9ee181e897
ProfManuel7 <profmanuel7@gmail.com>
2022-07-14 03:14:05 -0700

    7 leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,37 +1,28 @@
-/**
- * leet - a function that encodes a string into 1337
- *
- * @s: string input
- *
- * Return: @s
-*/
+#include "main.h"
 
+/**
+ *leet - encodes a string to leet
+ *
+ *@s:input
+ *
+ *Return:pointer s
+ *
+ */
 char *leet(char *s)
 {
-  int i, c = 0;
-  int sl[] = {97, 101, 111, 116, 108};
-  int ul[] = {65, 69, 79, 84, 76};
-  int n[] = {52, 51, 48, 55, 49};
+	int i, j;
 
-  /* iterate values in array s */
-  while (s[c] != '\0') 
-  {
-   /** 
-     * loop through array value five times
-     * to check if the value is equal to
-     * any small letter in array sl or
-     * upper letter in array ul and if it
-     * is replace it with the value in array n
-    */
-    for (i = 0; i < 5; i++)
-    {
-      if (s[c] == sl[i] || s[c] == ul[i])
-      {
-        s[c] = n[i];
-        break;
-      }
-    }
-    c++;
-  }
-  return (s);
+	char b[] = "ol_ea__t";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; b[j] != '\0'; j++)
+		{
+			if (s[i] == b[j] || s[i] == (b[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+	return (s);
 }
